43b8867ab8faf3ce907eb71e1928901a160034ec
Andrew Smith <meuzishun@gmail.com>
2022-06-20 13:57:12 -0400

    Create template for Footer.js and structure app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import Header from './components/header/Header';
+import Cart from './components/cart/Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/footer/Footer.js';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Cart />
+      <BrowserRouter>
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
