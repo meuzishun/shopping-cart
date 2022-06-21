dacffbac33f21e6f60516c7169ed3cc97242ff4a
Andrew Smith <meuzishun@gmail.com>
2022-06-21 16:50:50 -0400

    Add basic styles to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Nav from '../nav/Nav';
+import styles from './Header.module.css';
 
 function Header(props) {
   const { itemCount } = props;
   return (
-    <div>
+    <div className={styles.header}>
       <h1>logo</h1>
       <Nav itemCount={itemCount} />
       {console.log('Header has rendered...')}
