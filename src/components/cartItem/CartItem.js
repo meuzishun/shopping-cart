1d725f839e4ed270dc91bb1d0da3defcb06dd023
Andrew Smith <meuzishun@gmail.com>
2022-06-21 21:06:36 -0400

    Layout item content

--- src/components/cartItem/CartItem.js
+++ src/components/cartItem/CartItem.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import QuantityWidget from '../quantityWidget/QuantityWidget';
+import styles from './CartItem.module.css';
 
 function CartItem(props) {
   const { item, editItemQuantity } = props;
   const { name, description, price } = item;
+
   return (
-    <div>
-      <p>{name}</p>
-      <p>{description}</p>
-      <p>{price}</p>
+    <div className={styles.item}>
+      <h4 className={styles.title}>{name}</h4>
+      <p className={styles.description}>{description}</p>
+      <p className={styles.price}>${price}</p>
       <QuantityWidget item={item} editItemQuantity={editItemQuantity} />
     </div>
   );
