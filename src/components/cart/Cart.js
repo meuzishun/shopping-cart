c3f9af154187dd9bebacb875b5a85ba0b835ffb8
Andrew Smith <meuzishun@gmail.com>
2022-06-21 21:00:19 -0400

    Style cart a bit

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -9,16 +9,16 @@ function Cart(props) {
     <div className={styles.cart}>
       {/* {console.log('cart rendered...')} */}
       <h2>shopping cart</h2>
-      {/* <div className={styles.items}> */}
-      {/*TODO - this encap breaks the func */}
-      {cartItems.map((item) => (
-        <CartItem
-          key={item.id}
-          item={item}
-          editItemQuantity={editItemQuantity}
-        />
-      ))}
-      {/* </div> */}
+      <div className={styles.items}>
+        {/*TODO - this encap breaks the func */}
+        {cartItems.map((item) => (
+          <CartItem
+            key={item.id}
+            item={item}
+            editItemQuantity={editItemQuantity}
+          />
+        ))}
+      </div>
       <p>total price: ${totalPrice}</p>
       <button onClick={checkout}>checkout</button>
       {console.log('Cart has rendered...')}
