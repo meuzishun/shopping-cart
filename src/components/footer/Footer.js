2c566d9a01f10500719897d99a95f2ddff25426d
Andrew Smith <meuzishun@gmail.com>
2022-06-22 09:05:12 -0400

    Add basic styles to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <div>
+    <footer>
       <p>Copyright &copy; meuzishun 2022</p>
       {console.log('Footer has rendered...')}
-    </div>
+    </footer>
   );
 }
 
