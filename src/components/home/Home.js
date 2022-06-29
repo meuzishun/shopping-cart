5621256cab22b33a611b9ec5aae1ed5073fbd510
Andrew Smith <meuzishun@gmail.com>
2022-06-29 18:05:50 -0400

    Add some styles to home screen

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.module.css';
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <h1 className={styles.home_heading}>Audiophonics</h1>
+      <p className={styles.home_description}>
+        Get quality audio products at the best prices
+      </p>
+      <button className={styles.home_shop_btn}>
+        <Link to='shop'>Shop</Link>
+      </button>
+
       {/* {console.log('Home has rendered...')} */}
     </div>
   );
